0348ed17db9b8733e620ba1e787f4629088928b3
wanzx11 <2388824995@qq.com>
2024-10-26 16:08:29 +0800

    配置SpringSecurity基本内容

--- backend/src/main/java/com/exmple/BackendApplication.java
+++ backend/src/main/java/com/exmple/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.exmple.backend;
+package com.exmple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/main/java/com/exmple/entity/RestBean.java
@@ -0,0 +1,27 @@
+package com.exmple.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+
+public record RestBean<T>(int code, T data , String message) {
+
+    public static <T> RestBean<T> success(T data) {
+        return new RestBean<T>(200,data,"请求成功");
+    }
+
+    public static <T> RestBean<T> success() {
+        return success(null);
+    }
+
+    public static <T> RestBean<T> fail(int code,String message) {
+        return new RestBean<T>(code,null,message);
+    }
+
+
+
+    public String asJsonString(){
+     return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
+}
